3a5a47650fd150abbb57f420f7d9f27bbf0e39f0
Behnam <behnamborhani95@gmail.com>
2024-01-23 08:58:56 +0330

    add routes

---
+++ src/routes.tsx
@@ -0,0 +1,10 @@
+import App from "./App";
+import Cart from "./Pages/Cart";
+import Home from "./Pages/Home/Home";
+
+const routes = [
+  { path: "/", element: <Home /> },
+  { path: "/cart", element: <Cart /> },
+];
+
+export default routes;
